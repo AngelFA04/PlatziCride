ef2641ac398313647e8c9cd8196300d2dc9a78d2
Angel Flores <cyberangel1000@gmail.com>
2020-11-11 16:04:18 -0600

    Created admin manager for memberships

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -4,7 +4,7 @@
 from django.contrib import admin
 
 # Models
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 
 @admin.register(Circle)
 class CircleAdmin(admin.ModelAdmin):
@@ -21,4 +21,9 @@ class CircleAdmin(admin.ModelAdmin):
     search_fields = ('slug_name', 'name')
     list_filter = ('is_public', 'verified', 'is_limited')
 
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    """Membership admin"""
 
+    list_display = ('user', 'profile', 'circle', 'is_active')
+    search_fields = ('user__username', 'circle__slug_name')
