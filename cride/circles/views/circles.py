aaeac2b4d2b7db8af321bfb524958d165e6de51f
Angel Flores <cyberangel1000@gmail.com>
2020-11-10 19:26:36 -0600

    Created permissions for circles

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,13 +1,17 @@
 """ Circles views """
 
 # DRF
-from rest_framework import viewsets, mixins, permissions
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 # Models
 from cride.circles.models import Circle, Membership
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
+
+# Permissions
+from cride.circles.permissions import IsCircleAdmin
 
 class CircleViewSet(mixins.CreateModelMixin,
                     mixins.RetrieveModelMixin,
@@ -16,13 +20,20 @@ class CircleViewSet(mixins.CreateModelMixin,
                     GenericViewSet):
     """ Circle viewset """
     serializer_class = CircleModelSerializer
-    permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
         queryset = Circle.objects.all()
         if self.action == 'list':
             return queryset.filter(is_public=True)
         return queryset
+
+    def get_permissions(self):
+        """Assign permissions based on actions"""
+        permissions = [IsAuthenticated]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsCircleAdmin)
+
+        return [p() for p in permissions]
 
     def perform_create(self, serializer):
         """Assign circle admin."""
