ab37e6e7ff5b552ed799da22f562084592563680
Angel Flores <cyberangel1000@gmail.com>
2020-11-29 15:49:39 -0600

    Created more functionalities for rides app

--- cride/rides/views/rides.py
+++ cride/rides/views/rides.py
@@ -4,20 +4,33 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import get_object_or_404
 
 # Serializers
-from cride.rides.serializers import CreateRideSerializer
+from cride.rides.serializers import CreateRideSerializer, RideModelSerializer
+
+# Filters
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Permissions
 from cride.circles.permissions import IsActiveCircleMember
+from cride.rides.permissions import IsRideOwner
 
 # Models
 from cride.circles.models import Circle
 
-class RideViewSets(mixins.CreateModelMixin, viewsets.GenericViewSet):
+# Utils
+from django.utils import timezone
+from datetime import timedelta
+class RideViewSets(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.UpdateModelMixin,
+                   viewsets.GenericViewSet):
 
     """Ride view set."""
 
-    serializer_class =  CreateRideSerializer
     permission_classes = [IsAuthenticated, IsActiveCircleMember]
+    filter_backends = (SearchFilter, OrderingFilter)
+    ordering = ('departure_date', 'arrival_date', 'available_seats')
+    ordering_fields = ('departure_date', 'arrival_date', 'available_seats')
+    search_fields = ('departure_location', 'arrival_location')
 
     def dispatch(self, request, *args, **kwargs):
         """Verify that the circle exists"""
@@ -25,8 +38,33 @@ class RideViewSets(mixins.CreateModelMixin, viewsets.GenericViewSet):
         self.circle = get_object_or_404(Circle, slug_name=slug_name)
         return super(RideViewSets, self).dispatch(request, *args, **kwargs)
 
+    def get_permissions(self):
+        """Assign permission based on action."""
+        permissions = [IsAuthenticated, IsActiveCircleMember]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsRideOwner)
+
+        return [p() for p in permissions]
+
     def get_serializer_context(self):
         context = super(RideViewSets, self).get_serializer_context()
         context['circle'] = self.circle
 
         return context
+
+    def get_serializer_class(self):
+        """Return serializer based on action."""
+        if self.action == 'create':
+            return CreateRideSerializer
+
+        return RideModelSerializer
+
+    def get_queryset(self):
+        """Return active circles rides."""
+        offset = timezone.now() + timedelta(minutes=10)
+
+        return  self.circle.ride_set.filter(
+            departure_date__gte=offset,
+            is_active=True,
+            available_seats__gte=1,
+        )
